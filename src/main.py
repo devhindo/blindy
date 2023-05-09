2428509d695dbe9d330005fef0fc6698787e4740
hindawi <ahmedhindawi2001@gmail.com>
2023-05-09 21:06:46 +0300

    added additional methods

--- src/main.py
+++ src/main.py
@@ -12,3 +12,26 @@ def pdf_to_string(pdf_path):
 def pdf_to_mp3(pdf_path, mp3_path, lang='en', tld='co.uk'):
     mp3 = gTTS(text=pdf_to_string(pdf_path), lang=lang, tld='co.uk')
     mp3.save(mp3_path)
+
+def ls():
+    for cmd in cmds.keys():
+        print(cmd)
+
+def quit():
+    exit()
+
+# actual program
+
+cmds = {
+    'pdf-to-mp3': pdf_to_mp3,
+    'ls': ls,
+    'exit': quit
+}
+
+while(True):
+    cmd = input('Enter a command or (ls) for available commands: ')
+    args = cmd.split()
+    if args[0] in cmds:
+        cmds[args[0]](*args[1:])
+    else:
+        print('Invalid command')
